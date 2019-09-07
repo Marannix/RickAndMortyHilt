488bfb56b0cc468e9c4c2f95404102b26ab529ca
Josh <onabanjotobi@gmail.com>
2019-09-07 21:09:50 +0100

    Display characters in a reycler view

---
+++ app/src/main/java/com/example/rickandmorty/adapter/CharactersAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.rickandmorty.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.rickandmorty.R
+import com.example.rickandmorty.data.characters.CharactersResults
+import kotlinx.android.synthetic.main.characters_item.view.*
+
+class CharactersAdapter : RecyclerView.Adapter<CharactersAdapter.ViewHolder>() {
+
+    private var data: List<CharactersResults> = emptyList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.characters_item, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        if (data.isNotEmpty()) {
+            holder.bind(data[position])
+        }
+    }
+
+    fun setData(charactersResults: List<CharactersResults>) {
+        data = charactersResults
+        this.notifyDataSetChanged()
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(charactersResults: CharactersResults) {
+            itemView.characterName.text = charactersResults.name
+        }
+    }
+}
--- app/src/main/java/com/example/rickandmorty/fragment/CharactersFragment.kt
+++ app/src/main/java/com/example/rickandmorty/fragment/CharactersFragment.kt
@@ -1,25 +1,28 @@
 package com.example.rickandmorty.fragment
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.rickandmorty.R
+import com.example.rickandmorty.adapter.CharactersAdapter
+import com.example.rickandmorty.data.characters.CharactersResults
 import com.example.rickandmorty.repository.CharactersRepository
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.fragment_characters.*
 
 const val FIRST_PAGE = 1
 
 class CharactersFragment : Fragment() {
 
     private val disposables = CompositeDisposable()
+    private val charactersAdapter = CharactersAdapter()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_characters, container, false)
@@ -27,16 +30,27 @@ class CharactersFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        
+        charactersRecyclerView.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        charactersRecyclerView.adapter = charactersAdapter
 
         val disposable = CharactersRepository().fetchCharacters(FIRST_PAGE)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
-                { Toast.makeText(requireContext(), it.charactersResults[1].id, Toast.LENGTH_SHORT).show() },
-                { Toast.makeText(requireContext(), "yikes", Toast.LENGTH_SHORT).show() }
+                { onRetrieveCharactersSuccess(it.charactersResults) },
+                { onRetrieveCharactersError() }
             )
 
         disposables.add(disposable)
+    }
+
+    private fun onRetrieveCharactersSuccess(charactersResults: List<CharactersResults>) {
+        charactersAdapter.setData(charactersResults)
+    }
+
+    private fun onRetrieveCharactersError() {
+        Toast.makeText(requireContext(), "Something went wrong while retrieving characters", Toast.LENGTH_SHORT).show()
     }
 
     override fun onStop() {
