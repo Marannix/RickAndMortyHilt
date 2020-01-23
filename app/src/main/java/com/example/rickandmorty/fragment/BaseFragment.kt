744798c84bd77d3cfa43e37602d65da5a2ec034d
Josh <onabanjotobi@gmail.com>
2020-01-23 14:54:06 +0000

    Added episodes to adapter

--- app/src/main/java/com/example/rickandmorty/fragment/BaseFragment.kt
+++ app/src/main/java/com/example/rickandmorty/fragment/BaseFragment.kt
@@ -1,10 +1,25 @@
 package com.example.rickandmorty.fragment
 
 import android.os.Bundle
+import android.util.Log
 import dagger.android.support.DaggerFragment
+import io.reactivex.exceptions.UndeliverableException
+import io.reactivex.plugins.RxJavaPlugins
 
 abstract class BaseFragment : DaggerFragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        handleUndeliverableException()
+    }
+
+    private fun handleUndeliverableException() {
+        //Added due to -  2 exceptions occurred. which causes a crash
+        RxJavaPlugins.setErrorHandler { e ->
+            var error = ""
+            if (e is UndeliverableException) {
+                error = e.cause.toString()
+            }
+            Log.e("Whats going on", error)
+        }
     }
 }
--- app/src/main/java/com/example/rickandmorty/fragment/CharacterDetailsFragment.kt
+++ app/src/main/java/com/example/rickandmorty/fragment/CharacterDetailsFragment.kt
@@ -14,12 +14,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.rickandmorty.R
 import com.example.rickandmorty.adapter.EpisodeAdapter
 import com.example.rickandmorty.data.characters.CharactersResults
-import com.example.rickandmorty.data.network.EpisodeResponse
 import com.example.rickandmorty.viewmodel.EpisodesViewModel
 import com.squareup.picasso.Picasso
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.exceptions.UndeliverableException
-import io.reactivex.plugins.RxJavaPlugins
 import kotlinx.android.synthetic.main.character_header.*
 import kotlinx.android.synthetic.main.character_summary.*
 import kotlinx.android.synthetic.main.fragment_characters_detail.*
@@ -35,13 +31,8 @@ class CharacterDetailsFragment : BaseFragment() {
             .get(EpisodesViewModel::class.java)
     }
 
-    private val disposables = CompositeDisposable()
     private val episodesAdapter = EpisodeAdapter()
-
     private lateinit var character: CharactersResults
-    private val episodes = ArrayList<EpisodeResponse>()
-
-    private fun selector(episode: EpisodeResponse): Int = episode.id.toInt()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_characters_detail, container, false)
@@ -55,20 +46,7 @@ class CharacterDetailsFragment : BaseFragment() {
             character = safeArgs.character
         }
         viewModel.getEpisodes(character)
-        handleUndeliverableException()
         loadUI()
-    }
-
-    private fun handleUndeliverableException() {
-        //Added due to -  2 exceptions occurred. which causes a crash
-        RxJavaPlugins.setErrorHandler { e ->
-            var error = ""
-            if (e is UndeliverableException) {
-                error = e.cause.toString()
-            }
-            Log.e("Whats going on", error)
-
-        }
     }
 
     private fun loadUI() {
@@ -139,10 +117,7 @@ class CharacterDetailsFragment : BaseFragment() {
                     // Show Loading indicator
                 }
                 is EpisodesViewModel.ViewState.Content -> {
-                    Log.d(
-                        "I have content",
-                        viewState.listOfEpisodes.also { viewState.listOfEpisodes.sortedBy { selector(it) }}.toString()
-                    )
+                    episodesAdapter.setEpisodes(viewState.listOfEpisodes)
                 }
                 is EpisodesViewModel.ViewState.Error -> {
                     Log.d(
@@ -152,56 +127,10 @@ class CharacterDetailsFragment : BaseFragment() {
                 }
             }
         })
-//        viewModel.stuff(character.id, character.episode)
-//        for (i in character.episode) {
-//            val disposable = viewModel.fetchEpisodes("$i/")
-//                .subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe(
-//                    {
-//                        onRetrieveEpisodesSuccess(it)
-//                    },
-//                    {
-//                        onRetrieveEpisodesError()
-//                    }
-//                )
-//            disposables.add(disposable)
-//        }
     }
-
-    private fun onRetrieveEpisodesSuccess(episode: EpisodeResponse) {
-        // This should be handled in the viewmodel
-        episode.characterId = character.id
-        episodes.add(
-            EpisodeResponse(
-                episode.episodeId,
-                episode.characterId,
-                episode.id,
-                episode.name,
-                episode.airdate,
-                episode.episode
-            )
-        ).also {
-            episodes.sortBy { selector(it) }
-        }
-        episodesAdapter.setEpisodes(episodes)
-     //  viewModel.insertEpisodes(episode)
-    }
-//
-//    private fun onRetrieveEpisodesError() {
-//        if (shouldFetchEpisodes) {
-//            episodesAdapter.setEpisodes(viewModel.getEpisodes(character.id))
-//        }
-//        shouldFetchEpisodes = false
-//    }
 
     private fun updateToolbar() {
         (activity as? AppCompatActivity)?.supportActionBar?.title = getString(R.string.toolbar_overview_title)
-    }
-
-    override fun onStop() {
-        disposables.clear()
-        super.onStop()
     }
 
 }
