a4aebb71aec3246fce7b8c2358f7dd429e4a54bd
Joshua Onabanjo <joshua@memrise.com>
2020-01-24 14:14:13 +0000

    Added google navigation bottom bar

--- app/src/main/java/com/example/rickandmorty/activity/MainActivity.kt
+++ app/src/main/java/com/example/rickandmorty/activity/MainActivity.kt
@@ -1,15 +1,55 @@
 package com.example.rickandmorty.activity
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.rickandmorty.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
+    private lateinit var navController : NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        setNavController()
+        setupBottomNavMenu()
     }
 
+    private fun setNavController() {
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.destination_characters, R.id.destination_episodes-> {
+                    showBottomNav()
+                }
+                else -> hideBottomNav()
+            }
+        }
+    }
+
+    private fun setupBottomNavMenu() {
+        bottomNavigation.let {
+            NavigationUI.setupWithNavController(it, navController)
+        }
+    }
+
+    private fun showBottomNav() {
+        bottomNavigation.visibility = View.VISIBLE
+    }
+
+    private fun hideBottomNav() {
+        bottomNavigation.visibility = View.INVISIBLE
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navigated = NavigationUI.onNavDestinationSelected(item, navController)
+        return navigated || super.onOptionsItemSelected(item)
+    }
 }
